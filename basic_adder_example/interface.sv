b149088f2f5959030c3d96f02d6c91a9de1d2f74
Yiğit Demir <91564551+calliyigit@users.noreply.github.com>
2025-05-27 01:15:44 +0300

    Update interface.sv

--- basic_adder_example/interface.sv
+++ basic_adder_example/interface.sv
@@ -1,4 +1,10 @@
+// Interface definition for the adder DUT
 interface add_if(input logic clk, reset);
+
+  // 8-bit input signals to the adder
   logic [7:0] ip1, ip2;
+
+  // 9-bit output signal to accommodate overflow from 8-bit addition
   logic [8:0] out;
+
 endinterface
