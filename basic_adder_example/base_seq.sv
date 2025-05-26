d10b912f02024adaf1a93d5fb5489503fd87eb5b
Yiğit Demir <91564551+calliyigit@users.noreply.github.com>
2025-05-27 01:14:47 +0300

    Update base_seq.sv

--- basic_adder_example/base_seq.sv
+++ basic_adder_example/base_seq.sv
@@ -1,31 +1,33 @@
 class base_seq extends uvm_sequence#(seq_item);
+
+    // Register the sequence with the UVM factory
     `uvm_object_utils(base_seq)
 
-    seq_item req;
-    //Constructor
+    //seq_item req; // The sequence item that will be generated and sent
+
+    // Constructor
     function new(string name = "seq_item");
-        super.new(name);
+        super.new(name); // Call base class constructor
     endfunction
 
-    //Body 
+    // Main sequence body
     virtual task body();
-        `uvm_info(get_type_name(), "Base seq: Inside Body", UVM_LOW)
-        `uvm_do(req)
-        
-        // -------------------------------------------------------
-        //We can do these code instead of `uvm_do()
-        // -------------------------------------------------------        
-        //req = seq_item::type_id::create("req");
-        //start_item(req);
-        //if (!req.randomize()) begin
-        //    `uvm_error(get_type_name(), "Randomization Failed!")
-        //end
-        //finish_item(req);
-        // -------------------------------------------------------
-        
+        // Generate and send a sequence item using the `uvm_do` macro
+        `uvm_do(req);
 
+        // -------------------------------------------------------
+        // Alternatively, the above macro expands to the code below:
+        // -------------------------------------------------------
+        // req = seq_item::type_id::create("req"); // Create sequence item
+        // start_item(req);                         // Start the item
+        // if (!req.randomize()) begin              // Randomize it
+        //     `uvm_error(get_type_name(), "Randomization Failed!")
+        // end
+        // finish_item(req);                        // Complete the item
+        // -------------------------------------------------------
+
+        // Print out the generated item information
         `uvm_info(get_type_name(), $sformatf("Generated item: %s", req.convert2str()), UVM_LOW)
     endtask
 
-    
 endclass
