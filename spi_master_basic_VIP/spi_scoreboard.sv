27a8a75673cb8fcc8e6ca5e2ae684b0f9a29b843
Yiğit Demir <91564551+calliyigit@users.noreply.github.com>
2025-05-29 23:17:56 +0300

    Update spi_scoreboard.sv

--- spi_master_basic_VIP/spi_scoreboard.sv
+++ spi_master_basic_VIP/spi_scoreboard.sv
@@ -1,33 +1,70 @@
 class spi_scoreboard extends uvm_scoreboard;
     `uvm_component_utils(spi_scoreboard)
 
-    uvm_analysis_imp#(spi_seq_item, spi_scoreboard) item_collected_export;
+    //uvm_analysis_imp#(spi_seq_item, spi_scoreboard) item_collected_export;
+  	
+  	`uvm_analysis_imp_decl(_expected)
+	`uvm_analysis_imp_decl(_actual)
+  
+  	uvm_analysis_imp_expected #(spi_seq_item, spi_scoreboard) expected_ap;
+    uvm_analysis_imp_actual  #(spi_seq_item, spi_scoreboard) actual_ap;
+ 	
 
-    spi_seq_item pkt_qu[$];
+	//Driver ve monitorden gelen veriler queue si
+    spi_seq_item expected_q[$];
+    spi_seq_item actual_q[$];
 
-    //constructor
     function new(string name = "spi_scoreboard", uvm_component parent);
-      super.new(name, parent);
+        super.new(name, parent);
+        expected_ap = new("expected_ap", this);
+        actual_ap  = new("actual_ap", this);
     endfunction
 
-    //Build phase
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-        item_collected_export = new("item_collected_export", this);
+    //Driverden gelen
+    function void write_expected(spi_seq_item t);
+        expected_q.push_back(t);
+       
     endfunction
 
-    function void write(spi_seq_item item);
-        pkt_qu.push_back(item);
+    //Monitörden gelen
+    function void write_actual(spi_seq_item t);
+        actual_q.push_back(t);
+        
     endfunction
 
     task run_phase(uvm_phase phase);
         spi_seq_item item_collected;
+      	spi_seq_item item_expected;
         forever begin
-            wait(pkt_qu.size() > 0);
+            wait(expected_q.size() > 0 && actual_q.size() > 0);
 
-            item_collected = pkt_qu.pop_front;
+            item_collected = actual_q.pop_front;
+          	item_expected = expected_q.pop_front;
+          	
+          
+          //Compare
+          if (item_collected.mosi == item_expected.mosi_data_di && item_expected.miso_data == item_collected.miso) begin
+             $display("----------------------------------------------------------------");
+            `uvm_info("SCOREBOARD_MATCH", $sformatf("BASARILI: Beklenen MOSI verisi = %d, Gelen MOSI verisi = %d", item_collected.mosi, item_expected.mosi_data_di), UVM_LOW)
+            `uvm_info("SCOREBOARD_MATCH", $sformatf("MASTERE RANDOM SURDUGUMUZ VERI(slave gonderir) - %d VE MASTERIN EN SONDA SHIFT REGISTERINDE OLAN VERI - %d", item_expected.miso_data, item_collected.miso), UVM_LOW)
+            $display("----------------------------------------------------------------");
+            $display("----------------------------------------------------------------");
+            end else begin
+              if(item_collected.mosi != item_expected.mosi_data_di)begin
+                $display("----------------------------------------------------------------");
+                `uvm_error("SCOREBOARD_MISMATCH", $sformatf("HATA: Beklenen MOSI = %d, Gelen MOSI = %d", item_collected.mosi, item_expected.mosi_data_di))
+                $display("----------------------------------------------------------------");
+                $display("----------------------------------------------------------------");
+              end
+              if(item_expected.miso_data != item_collected.miso)begin
+                $display("----------------------------------------------------------------");
+                `uvm_error("SCOREBOARD_MISMATCH", $sformatf("HATA: Gonderilen MISO = %d, SHIFT REGTEKI MISO = %d", item_expected.miso_data, item_collected.miso))
+                $display("----------------------------------------------------------------");
+                $display("----------------------------------------------------------------");
+              end
+       
+            end
 
-          `uvm_info("SCOREBOARD", $sformatf("Collected data: %d", item_collected.mosi), UVM_LOW)
         end
     endtask
 endclass
